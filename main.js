4488c27760345d23436c994e2dc9df0310d71117
sahar-cohen <saharc21@gmail.com>
2023-07-02 09:43:42 +0300

    changed to countdown

--- main.js
+++ main.js
@@ -1,28 +1,28 @@
-// const countdown = document.querySelector(".countdown");
+const countdown = document.querySelector(".countdown");
 
-// const targetDate = new Date("July 30, 2023 00:00:00").getTime();
+const targetDate = new Date("July 30, 2023 00:00:00").getTime();
 
-// function updateCountdown() {
-//   const now = new Date().getTime();
-//   const timeRemaining = targetDate - now;
+function updateCountdown() {
+  const now = new Date().getTime();
+  const timeRemaining = targetDate - now;
 
-//   const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
-//   const hours = Math.floor(
-//     (timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-//   );
-//   const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
-//   const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+  const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
 
-//   document.getElementById("שניות").innerText = seconds;
-//   document.getElementById("דקות").innerText = minutes;
-//   document.getElementById("שעות").innerText = hours;
-//   document.getElementById("ימים").innerText = days;
-// }
+  document.getElementById("שניות").innerText = seconds;
+  document.getElementById("דקות").innerText = minutes;
+  document.getElementById("שעות").innerText = hours;
+  document.getElementById("ימים").innerText = days;
+}
 
-// updateCountdown();
-// setInterval(updateCountdown, 1000);
+updateCountdown();
+setInterval(updateCountdown, 1000);
 
-// // Theme switching code
+// Theme switching code
 // let isDay = true;
 
 // function setWebsiteTheme() {
